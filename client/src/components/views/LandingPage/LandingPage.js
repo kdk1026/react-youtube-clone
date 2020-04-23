4771aa11f08237c5ad0d5f83988bf1b63ea85ef9
kdk1026 <kdk1026@gmail.com>
2020-04-23 18:05:49 +0900

    오타 수정

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -29,7 +29,7 @@ function LandingPage() {
         var seconds = Math.floor(video.duration - minutes * 60);
 
         return <Col lg={6} md={8} xs={24}>
-            <a href={`/video/post/${video.id}`} >
+            <a href={`/video/post/${video._id}`} >
                 <div style={{ position: 'relative' }}>
                 <img style={{ width: '100%' }} alt="thumbnail" src={`http://localhost:5000/${video.thumbnail}`} />
                     <div className="duration">
@@ -48,7 +48,7 @@ function LandingPage() {
             <span>{video.writer.name} </span><br/>
             <span style={{ marginLeft: '3rem' }}> {video.views}</span> - <span> {moment(video.createdAt).format("MMM Do YY")} </span>
         </Col>
-        
+
     });
 
     return (
